18b3ae954d4bd4686321fb8500e9b431123a3a17
aqwarius2003 <00victorr@gmail.com>
2024-09-15 21:44:51 +0400

    Download books with titles

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import requests
 import os
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
-import re
+from pathvalidate import sanitize_filename
+
 
 def check_for_redirect(response):
     if response.history:
         raise HTTPError(f"Редирект на URL: {response.url}")
 
-
-def save_book(response, path):
-    with open(path, 'wb') as file:
-        file.write(response.content)
 
 def get_book_info(book_id):
     url = f'https://tululu.org/b{book_id}'
@@ -30,32 +27,33 @@ def get_book_info(book_id):
     except AttributeError:
         print(f"Изображение книги {book_id =} не найдено")
 
-    print(f'{book_title=},\n{book_author=},\n{book_url_img=}')
     return book_title, book_author, book_url_img
 
-def main():
 
+def download_txt(response, title, book_id, folder='books/'):
+    filename = f'{book_id}.{sanitize_filename(title)}.txt'
+    with open(os.path.join(folder, filename), 'wb') as file:
+        file.write(response.content)
+
+
+def main():
     url = f'https://tululu.org/txt.php'
     os.makedirs('books', exist_ok=True)
 
-    for book_id in range(1):
-        book_id = 32168
+    for book_id in range(11):
         params = {
             'id': book_id
         }
 
-        get_book_info(book_id)
         try:
             response = requests.get(url, params=params, allow_redirects=True)
             response.raise_for_status()
             check_for_redirect(response)
-
-
-
         except HTTPError as e:
             print(f"Ошибка при запросе книги {book_id}: {e}")
             continue
-        save_book(response=response, path=f'books/id_{book_id}.txt')
+        book_title, book_author, book_url_img = get_book_info(book_id)
+        download_txt(response, book_title, book_id)
 
 
 if __name__ == "__main__":
