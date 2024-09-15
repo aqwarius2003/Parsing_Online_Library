390d18014fb7f294ed48df584a6622213b1c7eb9
aqwarius2003 <00victorr@gmail.com>
2024-09-15 17:06:20 +0400

    Check_for_redirect

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import requests
 import os
-
-os.makedirs('books', exist_ok=True)
-url = f'https://tululu.org/txt.php'
+from requests.exceptions import HTTPError
 
 
-def download_book(url, path, params):
-    response = requests.get(url, params=params)
-    response.raise_for_status()
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError(f"Редирект на URL: {response.url}")
+
+
+def save_book(response, path):
     with open(path, 'wb') as file:
         file.write(response.content)
 
 
-for book_id in range(11):
-    params = {
-        'id': book_id
-    }
-    download_book(url=url,
-                  path=f'books/id_{book_id}.txt',
-                  params=params)
-os.makedirs('books', exist_ok=True)
+def main():
+    url = f'https://tululu.org/txt.php'
+    os.makedirs('books', exist_ok=True)
+
+    for book_id in range(11):
+        params = {
+            'id': book_id
+        }
+
+        try:
+            response = requests.get(url, params=params, allow_redirects=True)
+            response.raise_for_status()
+            check_for_redirect(response)
+        except HTTPError as e:
+            print(f"Ошибка при запросе книги {book_id}: {e}")
+            continue
+        save_book(response=response, path=f'books/id_{book_id}.txt')
+
+
+if __name__ == "__main__":
+    main()
