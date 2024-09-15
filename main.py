f151edc6aa063db22a6001483289caba0c886e11
aqwarius2003 <00victorr@gmail.com>
2024-09-15 23:06:09 +0400

    Download book covers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit, unquote
 
 
 def check_for_redirect(response):
@@ -11,8 +12,8 @@ def check_for_redirect(response):
 
 
 def get_book_info(book_id):
-    url = f'https://tululu.org/b{book_id}'
-    response = requests.get(url)
+    url = 'https://tululu.org'
+    response = requests.get(f'{url}/b{book_id}')
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find('h1').text
     if title_tag:
@@ -23,7 +24,8 @@ def get_book_info(book_id):
         print('Заголовок H1 не найден')
 
     try:
-        book_url_img = soup.find(class_="bookimage").find('img')['src']
+        book_src_img = soup.find(class_="bookimage").find('img')['src']
+        book_url_img = urljoin(url, book_src_img)
     except AttributeError:
         print(f"Изображение книги {book_id =} не найдено")
 
@@ -36,9 +38,30 @@ def download_txt(response, title, book_id, folder='books/'):
         file.write(response.content)
 
 
+def download_image(book_url_img, book_id, folder='images/'):
+    try:
+        response = requests.get(book_url_img)
+        response.raise_for_status()
+
+        split_url = urlsplit(book_url_img)
+        path = unquote(split_url.path)
+        extension = path.split('.')[-1] if '.' in path else None
+        image_name = f'{book_id}.{extension}' if 'nopic.gif' not in path else 'nopic.gif'
+
+        save_path = os.path.join(folder, image_name)
+
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+
+        print(f"Скачана обложка книги {book_id}")
+    except requests.RequestException as e:
+        print(f"Ошибка при скачивании обложки книги {book_id}: {e}")
+
+
 def main():
     url = f'https://tululu.org/txt.php'
     os.makedirs('books', exist_ok=True)
+    os.makedirs('images', exist_ok=True)
 
     for book_id in range(11):
         params = {
@@ -54,6 +77,7 @@ def main():
             continue
         book_title, book_author, book_url_img = get_book_info(book_id)
         download_txt(response, book_title, book_id)
+        download_image(book_url_img, book_id)
 
 
 if __name__ == "__main__":
