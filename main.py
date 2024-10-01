926d4da63a438799ad8d72b375ce7da873d73d9b
aqwarius2003 <00victorr@gmail.com>
2024-10-01 13:56:47 +0400

    download comments

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def get_book_info(book_id):
-    url = 'https://tululu.org'
-    response = requests.get(f'{url}/b{book_id}')
+def get_book_info(url, book_id):
+    url = f'{url}/b{book_id}/'
+    response = requests.get(url, allow_redirects=True)
+    response.raise_for_status()
+    check_for_redirect(response)
+
     soup = BeautifulSoup(response.text, 'lxml')
     title_tag = soup.find('h1').text
     if title_tag:
@@ -21,14 +24,27 @@ def get_book_info(book_id):
         book_author = title_author[1].strip()
     else:
         logger.info('Заголовок H1 не найден')
+        return None, None, None
 
-    try:
-        book_src_img = soup.find(class_="bookimage").find('img')['src']
-        book_url_img = urljoin(url, book_src_img)
-    except AttributeError:
-        logger.info(f"Изображение книги {book_title}-{book_author}- не найдено")
+    # comments = soup.find_all(class_='texts').find(spanclass="black")
+    comments = []
+    for comment in soup.find_all(class_='texts'):
+        span = comment.find('span', class_='black')
+        if span:
+            comments.append(span.text)
 
-    return book_title, book_author, book_url_img
+    # print(comments)
+
+    book_src_img = soup.find(class_="bookimage").find('img')['src']
+    book_url_img = urljoin(url, book_src_img)
+
+    return book_title, book_author, book_url_img, comments
+
+
+def check_for_redirect(response):
+    """Проверяет, есть ли редирект. Если есть - выдаст ошибку"""
+    if response.history:
+        raise HTTPError(f"Редирект на URL: {response.url}")
 
 
 def download_txt(url, filename, folder='books/'):
@@ -60,7 +76,6 @@ def download_txt(url, filename, folder='books/'):
 
 
 def download_image(book_url_img, book_id, folder='images/'):
-
     response = requests.get(book_url_img)
     response.raise_for_status()
 
@@ -75,46 +90,30 @@ def download_image(book_url_img, book_id, folder='images/'):
     with open(save_path, 'wb') as file:
         file.write(response.content)
 
-    logger.info(f"Скачана обложка книги {book_id}")
+    return save_path
 
+    # logger.info(f"Скачана обложка книги {book_id}")
 
 
 def main():
-    url = 'http://tululu.org/txt.php?id=1'
+    url = f'https://tululu.org'
+    os.makedirs('images', exist_ok=True)
+    url_txt = f'https://tululu.org/txt.php'
 
-    filepath = download_txt(url, 'Алиби')
-    print(filepath)  # Выведется books/Алиби.txt
-
-    filepath = download_txt(url, 'Али/би', folder='books/')
-    print(filepath)  # Выведется books/Алиби.txt
-
-    filepath = download_txt(url, 'Али\\би', folder='txt/')
-    print(filepath)  # Выведется txt/Алиби.txt
-
-
-    # url = f'https://tululu.org/txt.php'
-    # os.makedirs('books', exist_ok=True)
-    # os.makedirs('images', exist_ok=True)
-
-    # for book_id in range(11):
-    #     params = {
-    #         'id': book_id
-    #     }
-    #
-    #     try:
-    #         response = requests.get(url, params=params, allow_redirects=True)
-    #         response.raise_for_status()
-    #         if response.history:
-    #             continue
-    #         book_title, book_author, book_url_img = get_book_info(book_id)
-    #         logger.info(f'Заголовок:{book_title}\n{book_url_img}')
-    #         filename = f'{book_id}.{sanitize_filename(book_title)}.txt'
-    #         download_txt(response, url, filename)
-    #         download_image(book_url_img, book_id)
-    #     except HTTPError as e:
-    #         logger.info(f"Ошибка при запросе книги: {e}")
-    #         continue
+    for book_id in range(11):
+        try:
+            book_title, book_author, book_url_img, comments = get_book_info(url, book_id)
+            url_txt = f'{url_txt}?id{book_id}'
+            filename = f'{book_id}.{book_title}'
+            path_txt_file = download_txt(url_txt, filename)
+            path_image = download_image(book_url_img, book_id)
+            logger.info(f'\nАвтор: {book_author}\nЗаголовок: {book_title}\nИзображение: {book_url_img}\n'
+                        f'Пути к скачанным файлам:\n{path_txt_file}\n{path_image}\n{comments}\n\n')
+        except HTTPError as e:
+            # logger.info(f"Ошибка при запросе книги: {e}")
+            continue
 
 
 if __name__ == "__main__":
     main()
+# https://tululu.org/id=5
